e4c286185b901965970573fdf0d4e5fb10e83ecc
ZinLinnhtet <mackk5504@gmail.com>
2026-04-06 16:45:22 +0700

    Feat: CRUD BLOCKS

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"crypto/md5"
+	"crypto/sha256"
+	"io"
+	"fmt"
+	"time"
+	"encoding/json"
+	"encoding/hex"
 )
 
 type Block struct {
@@ -34,6 +41,28 @@ type BlockChain struct {
 }
 
 var blockchain *BlockChain
+
+func newBook(w http.ResponseWriter, r *http.Request){
+	var book Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not decode JSON: %v", err)
+		w.Write([]byte("Could not create a new book"))
+		return
+	}
+	h := md5.New()
+	io.WriteString(h, book.ISBN + book.PublishedDate)
+	book.Id = fmt.Sprintf("%x", h.Sum(nil))
+	
+}
+
+func writeBlocks(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func getBlockchain(w http.ResponseWriter, r *http.Request){
+	
+}
 
 func main(){
 	r := mux.NewRouter()
